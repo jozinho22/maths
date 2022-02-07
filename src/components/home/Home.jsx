dbc93f1f9c0bf1da5571c56c704e44cee3bc1758
jozinho <josselin.douineau.1987@gmail.com>
2022-02-07 19:22:13 +0100

    shuffle Home exp + functions attributes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,36 +7,167 @@ const Home = () => {
     var dLnx = "\\(\\frac{d(\\ln{x})}{dx} = \\frac{1}{x}\\)";
     var pyth = "\\(x^2 + y^2 = z^2\\)";
     var dSqrt = "\\(\\frac{d(\\sqrt{x^2+1})}{dx} = \\frac{x}{\\sqrt{x^2+1}}\\)";
-    var tauxVar = "\\(\\frac{d(f(x))}{dx} = \\lim_{h \\to 0 } \\frac{f(x+h)-f(x)}{h}\\)"
+    var tauxVar = "\\(\\frac{d(f(x))}{dx} = \\lim_{h \\to 0} \\frac{f(x+h)-f(x)}{h}\\)"
     var sinCos = "\\(cos{x^2(x)} + sin{x^2(x)} = 1\\)"
     var integX2 = "\\(\\int x^2 dx = \\frac{1}{3} x^3\\)"
     var alKashi = "\\(c = \\sqrt{a^2 + b^2 - 2abcos\\gamma}\\)"
     var distrib = "\\( a(b+c) = ab + ac \\)"
     var distrib2 = "\\( a^2 - b^2 = (a+b)(a-b) \\)"
-
     var powTimesPow = "\\(x^a x^b = x^{a+b}\\)";
-    var powPowPow = "\\( (x^a)^b = x^{ab}\\)";
+    var powPow = "\\( (x^a)^b = x^{ab}\\)";
     var intSqrtX2Minus1 = "\\(\\int \\frac{1}{\\sqrt{x^2+1}} dx = arccosh(x)\\)";
     var dExpX = "\\(\\frac{d(e^{x})}{dx} = e^{x}\\)";
     var dX3 = "\\(\\frac{d(x^{3})}{dx} = 3 x^{2}\\)";
-    var dsin = "\\(\\frac{d(sin(x))}{dx} = cos(x)\\)";
-
-
+    var dSin = "\\(\\frac{d(sin(x))}{dx} = cos(x)\\)";
     var triangleArea = "\\(S_{triangle} = \\frac{bh}{2}\\)"
     var cubeVolume = "\\(V_{cube} = c^3\\)"
     var coneVolume = "\\(V_{cône} = \\pi r^2 \\frac{h}{3}\\)"
     var cylVolume = "\\(V_{cylindre} = \\pi r^2 h\\)"
     var sphVolume = "\\(V_{sphère} = \\frac{4}{3}\\pi r^3\\)"
-
-
     var circleArea = "\\(S_{cercle} = \\pi r^2\\)"
     var circlePerim = "\\(P_{cercle} = 2\\pi r\\)"
     var cosAPlusB = "\\(cos(a+b) = cos(a)cos(b) - sin(a)sin(b)\\)"
     var dUdivV = "\\((\\frac{u}{v})' = \\frac{u' v - v' u}{v^2}\\)";
     var dUplusV = "\\((u+v)' = u' + v' \\)";
     var dUV = "\\((uv)' = u'v + v'u \\)";
+    var phi2 = "\\(\\phi^2 = 2\\phi + 1\\)";
 
-    var phi2 = "\\(\\phi^2 = 2\\phi + 1\\)"
+    var listEqu = [];
+    var k = 0;
+    listEqu.push(
+            {
+                id: k++,
+                exp: dLnx
+            },
+            {
+                id: k++,
+                exp: pyth
+            },
+            {
+                id: k++,
+                exp: dSqrt
+            },
+            {
+                id: k++,
+                exp: tauxVar
+            },
+            {
+                id: k++,
+                exp: sinCos
+            },
+            {
+                id: k++,
+                exp: integX2
+            },
+            {
+                id: k++,
+                exp: alKashi
+            },
+            {
+                id: k++,
+                exp: dLnx
+            },
+            {
+                id: k++,
+                exp: distrib
+            },
+            {
+                id: k++,
+                exp: distrib2
+            },
+            {
+                id: k++,
+                exp: powTimesPow
+            },
+            {
+                id: k++,
+                exp: powPow
+            },
+            {
+                id: k++,
+                exp: intSqrtX2Minus1
+            },
+            {
+                id: k++,
+                exp: dExpX
+            },
+            {
+                id: k++,
+                exp: dX3
+            },
+            {
+                id: k++,
+                exp: dSin
+            },
+            {
+                id: k++,
+                exp: triangleArea
+            },
+            {
+                id: k++,
+                exp: cubeVolume
+            },
+            {
+                id: k++,
+                exp: coneVolume
+            },
+            {
+                id: k++,
+                exp: cylVolume
+            },
+            {
+                id: k++,
+                exp: sphVolume
+            },
+            {
+                id: k++,
+                exp: circleArea
+            },
+            {
+                id: k++,
+                exp: circlePerim
+            },
+            {
+                id: k++,
+                exp: cosAPlusB
+            },
+            {
+                id: k++,
+                exp: dUdivV
+            },
+            {
+                id: k++,
+                exp: dUplusV
+            },
+            {
+                id: k++,
+                exp: dUV
+            },
+            {
+                id: k++,
+                exp: phi2
+            }
+    );
+
+    shuffle(listEqu);
+
+    function shuffle(array) {
+        let currentIndex = array.length, randomIndex;
+        
+        // While there remain elements to shuffle...
+        while (currentIndex != 0) {
+        
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+        
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = 
+            [array[randomIndex], array[currentIndex]];
+        }
+        
+        return array;
+    }
 
     return (
         <Container className="HomeContainer">
@@ -45,35 +176,11 @@ const Home = () => {
             </Container>
 
             <Row>
-                <Col><MathJaxDisplay toShow={integX2} /></Col>
-                <Col><MathJaxDisplay toShow={pyth} /></Col>
-                <Col><MathJaxDisplay toShow={alKashi} /></Col>
-                <Col><MathJaxDisplay toShow={dSqrt} /></Col>
-                <Col><MathJaxDisplay toShow={dLnx} /></Col>
-                <Col><MathJaxDisplay toShow={distrib} /></Col>
-                <Col><MathJaxDisplay toShow={circleArea} /></Col>
-                <Col><MathJaxDisplay toShow={powTimesPow} /></Col>
-                <Col><MathJaxDisplay toShow={cubeVolume} /></Col>
-                <Col><MathJaxDisplay toShow={powTimesPow} /></Col>
-                <Col><MathJaxDisplay toShow={triangleArea} /></Col>
-                <Col><MathJaxDisplay toShow={tauxVar} /></Col>
-                <Col><MathJaxDisplay toShow={circlePerim} /></Col>
-                <Col><MathJaxDisplay toShow={intSqrtX2Minus1} /></Col>
-                <Col><MathJaxDisplay toShow={sinCos} /></Col>
-                <Col><MathJaxDisplay toShow={powPowPow} /></Col>
-                <Col><MathJaxDisplay toShow={cosAPlusB} /></Col>
-                <Col><MathJaxDisplay toShow={dExpX} /></Col>
-                <Col><MathJaxDisplay toShow={dX3} /></Col>
-                <Col><MathJaxDisplay toShow={coneVolume} /></Col>
-                <Col><MathJaxDisplay toShow={dsin} /></Col>
-                <Col><MathJaxDisplay toShow={distrib2} /></Col>
-                <Col><MathJaxDisplay toShow={cylVolume} /></Col>
-                <Col><MathJaxDisplay toShow={dUdivV} /></Col>
-                <Col><MathJaxDisplay toShow={phi2} /></Col>
-                <Col><MathJaxDisplay toShow={sphVolume} /></Col>
-                <Col><MathJaxDisplay toShow={dUV} /></Col>
-                <Col><MathJaxDisplay toShow={dUplusV} /></Col>
-
+                {listEqu.map(eq => (
+                    <Col key={eq.id}>
+                        <MathJaxDisplay toShow={eq.exp} />
+                    </Col>
+                ))}
             </Row>  
         </Container>
     );
--- src/components/usual-functions/UsualFunctions.jsx
+++ src/components/usual-functions/UsualFunctions.jsx
@@ -20,12 +20,29 @@ const UsualFunctions2 = () => {
     } 
 
     var i = -1;
+    var j = -1;
     var usualFunctions = [
         {
             id: ++i,
-            mathJaxTitle: "\\(x^2 \\)",
+            mathJaxTitle: "x^2",
             color: getFunctionColor(i),
             formula: (x) => {return x*x},
+            definition: "\\(\\xi = \\mathbb{R} = ]-\\infty, +\\infty[ \\)",
+            derivative: "2x",
+            primitive: "\\frac{x^3}{3} + K",
+            limits: [
+                {
+                    id: j++,
+                    where: "-\\infty",
+                    value: "+\\infty"
+                },
+                {
+                    id: j++,
+                    where: "+\\infty",
+                    value: "+\\infty"
+                }
+
+            ],
             scale : 10,
             step : 1,
             height: 300,
@@ -33,9 +50,25 @@ const UsualFunctions2 = () => {
         },
         {
             id: ++i,
-            mathJaxTitle: "\\(x^3 \\)",
+            mathJaxTitle: "x^3",
             color: getFunctionColor(i),
             formula: (x) => {return x*x*x},
+            definition: "\\(\\xi = \\mathbb{R} = ]-\\infty, +\\infty[ \\)",
+            derivative: "3x^2",
+            primitive: "\\frac{x^4}{4} + K",
+            limits: [
+                {
+                    id: j++,
+                    where: "-\\infty",
+                    value: "-\\infty"
+                },
+                {
+                    id: j++,
+                    where: "+\\infty",
+                    value: "+\\infty"
+                }
+
+            ],
             scale : 10,
             step : 1,
             height: 400,
@@ -43,10 +76,21 @@ const UsualFunctions2 = () => {
         },
         {
             id: ++i,
-            mathJaxTitle: "\\(\\sqrt(x) \\)",
+            mathJaxTitle: "\\sqrt(x)",
             color: getFunctionColor(i),
             formula: (x) => {return Math.sqrt(x)},
-            scale : 30,
+            definition: "\\(\\xi = \\mathbb{R^+} = [ 0, +\\infty[ \\)",
+            derivative: "\\frac{1}{2\\sqrt{x}}",
+            primitive: "\\frac{2x^{\\frac{3}{2}}}{3} + K",
+            limits: [
+                {
+                    id: j++,
+                    where: "+\\infty",
+                    value: "+\\infty"
+                }
+
+            ],
+            scale : 100,
             step : 1,
             height: 200,
             xInterval: 9,
@@ -54,9 +98,25 @@ const UsualFunctions2 = () => {
         },
         {
             id: ++i,
-            mathJaxTitle: "\\(e^x \\)",
+            mathJaxTitle: "e^x",
             color: getFunctionColor(i),
             formula: (x) => {return Math.exp(x)},
+            definition: "\\(\\xi = \\mathbb{R} = ]-\\infty, +\\infty[ \\)",
+            derivative: "e^x",
+            primitive: "e^x+ K",
+            limits: [
+                {
+                    id: j++,
+                    where: "-\\infty",
+                    value: "-\\infty"
+                },
+                {
+                    id: j++,
+                    where: "+\\infty",
+                    value: "+\\infty"
+                }
+
+            ],
             scale : 3,
             step : 1,
             height: 300,
@@ -64,21 +124,61 @@ const UsualFunctions2 = () => {
         },
         {
             id: ++i,
-            mathJaxTitle: "\\(ln(x) \\)",
+            mathJaxTitle: "ln(x)",
             color: getFunctionColor(i),
             formula: (x) => {return Math.log(x)},
-            scale : 50,
-            step : 1,
+            definition: "\\(\\xi = \\mathbb{R^{+*}} = ] 0, +\\infty[ \\)",
+            derivative: "\\frac{1}{x}",
+            primitive: "x(ln(x) - 1) + K",
+            limits: [
+                {
+                    id: j++,
+                    where: "0",
+                    value: "-\\infty"
+                },
+                {
+                    id: j++,
+                    where: "+\\infty",
+                    value: "+\\infty"
+                }
+            ],
+            scale : 10,
+            step : 0.1,
             height: 200,
-            xInterval: 8,
+            xInterval: 10,
             beginAtZero: true,
             exclude: 0
         },
         {
             id: ++i,
-            mathJaxTitle: "\\(\\frac{1}{x} \\)",
+            mathJaxTitle: "\\frac{1}{x}",
             color: getFunctionColor(i),
             formula: (x) => {return 1 / x},
+            definition: "\\(\\xi = \\mathbb{R} - \\{0\\} = \\mathbb{R^{*}} =  ]-\\infty, 0[\\bigcup] 0, +\\infty[ \\)",
+            derivative: "-\\frac{1}{x^2}",
+            primitive: "ln(|x|) + K",
+            limits: [
+                {
+                    id: j++,
+                    where: "-\\infty",
+                    value: "0_{-}"
+                },
+                {
+                    id: j++,
+                    where: "0_{-}",
+                    value: "-\\infty"
+                },
+                {
+                    id: j++,
+                    where: "0_{+}",
+                    value: "+\\infty"
+                },
+                {
+                    id: j++,
+                    where: "+\\infty",
+                    value: "0_{+}"
+                }
+            ],
             scale : 100,
             step : 1,
             height: 300,
@@ -87,9 +187,12 @@ const UsualFunctions2 = () => {
         },
         {
             id: ++i,
-            mathJaxTitle: "\\(\\cos(x) \\)",
+            mathJaxTitle: "cos(x)",
             color: getFunctionColor(i),
             formula: (x) => {return Math.cos(x)},
+            definition: "\\(\\xi = \\mathbb{R} = ]-\\infty, +\\infty[ \\)",
+            derivative: "-sin(x)",
+            primitive: "sin(x) + K",
             type: 'trigo',
             scale : 2,
             step : 1/4,
@@ -98,9 +201,12 @@ const UsualFunctions2 = () => {
         },
         {
             id: ++i,
-            mathJaxTitle: "\\(\\sin(x) \\)",
+            mathJaxTitle: "sin(x)",
             color: getFunctionColor(i),
             formula: (x) => {return Math.sin(x)},
+            definition: "\\(\\xi = \\mathbb{R} = ]-\\infty, +\\infty[ \\)",
+            derivative: "cos(x)",
+            primitive: "-cos(x) + K",
             type: 'trigo',
             scale : 2,
             step : 1/4,
@@ -127,10 +233,13 @@ const UsualFunctions2 = () => {
 
         var it = scale / step;
 
-        var x = 0;
+        var x;
         if(!f.beginAtZero) {
             x = -scale;
-        } 
+        } else {
+            x = 0;
+            it = it / 2;
+        }
 
         var xTrigo = -2*step;
 
@@ -140,18 +249,16 @@ const UsualFunctions2 = () => {
             if(f.type === 'trigo') {
                 let d = {
                     x: (xTrigo === 0) ? 0 : xTrigo +'π',
-                    y: f.formula(xTrigo*Math.PI),
+                    y: f.formula(xTrigo*Math.PI)
                 }
                 datas.push(d);
                 xTrigo = xTrigo + step;
             } else {
-                if(x !== f.exclude) {
-                    let d = {
-                        x: Math.floor(x),
-                        y: f.formula(x),
-                    }
-                    datas.push(d);
+                let d = {
+                    x: Math.floor(x),
+                    y: x !== f.exclude ? f.formula(x) : undefined
                 }
+                datas.push(d);
                 x = x + step;
             }
             
@@ -172,11 +279,26 @@ const UsualFunctions2 = () => {
 
     }, [f]);
 
-    const getGraphTitle = (title) => {
-        var index = title.indexOf("(");
-        var graphTitle = title.slice(0, index + 1) + "f(x) = " + title.slice(index + 1);
+    const getButtonTitle = (exp) => {
+        return "\\(" + exp + " \\)";
+    }
 
-        return graphTitle;
+    const getGraphTitle = (exp) => {
+        return "\\(f(x) = " + exp + " \\)";
+    }
+
+    const getDerivativeExp = (derivative) => {
+        return "\\(\\frac{d(f(x))}{dx} = " + derivative + " \\)";
+    }
+
+    const getPrimitiveExp = (primitive) => {
+        return "\\(\\int f(x) \\mathrm{d}x = " + primitive + " \\)";
+    }
+
+    const getLimitExp = (where, value) => {
+        var derivExp = "\\(\\lim_{x \\to " + where + "} f(x) = " + value + " \\)";
+        console.log(derivExp)
+        return derivExp;
     }
 
     function isEmpty(obj) {
@@ -191,7 +313,7 @@ const UsualFunctions2 = () => {
                         <Button key={f.id}
                             className={`DefaultButton InColumnButton ${f.color}Button`}
                             onClick={() => setF(usualFunctions[f.id])} >
-                            <MathJaxDisplay toShow={f.mathJaxTitle} />
+                            <MathJaxDisplay toShow={getButtonTitle(f.mathJaxTitle)} />
                         </Button>
                     ))
                 }
@@ -202,7 +324,7 @@ const UsualFunctions2 = () => {
                     <Container className="GraphTitle">
                         <MathJaxDisplay 
                             toShow={getGraphTitle(f.mathJaxTitle)} 
-                            color={f.color}/>
+                            color={f.color}/>    
                     </Container>
                     
                     <div style={styles}>
@@ -219,13 +341,47 @@ const UsualFunctions2 = () => {
                             <XAxis 
                                 dataKey="x" 
                                 interval={f.xInterval} />
-                            <YAxis />
+                            <YAxis 
+                                 />
                             <ReferenceLine x={0} />
                             <ReferenceLine y={0} />
                         </LineChart>
                     </div>
+                    <Container className="FunctionAttributes">
+                        <p className="FunctionAttributesTitle">Définie sur</p> 
+                        <MathJaxDisplay 
+                            toShow={f.definition} 
+                            color={f.color}/>  
+
+                        <p className="FunctionAttributesTitle">Dérivée</p> 
+                        <MathJaxDisplay 
+                            toShow={getDerivativeExp(f.derivative)} 
+                            color={f.color}/>    
+
+                        <p className="FunctionAttributesTitle">Primitives</p> 
+                        <MathJaxDisplay 
+                            toShow={getPrimitiveExp(f.primitive)} 
+                            color={f.color}/> 
+
+                        <p className="FunctionAttributesTitle">Limites</p> 
+                        {
+                            f.limits !== undefined ?
+                                
+                                f.limits.map(limit => (
+                                    <MathJaxDisplay 
+                                        key={limit.id}
+                                        toShow={getLimitExp(limit.where, limit.value)} 
+                                        color={f.color}/> 
+                                )) 
+                                
+                                    : <MathJaxDisplay 
+                                            toShow={"\\(\\varnothing\\)"} 
+                                            color={f.color}/> 
+                        }
+                   
+                    </Container>
                 </>
-                : <></>
+                : <p>Choisissez une fonction à afficher</p>
             }
         </>
     );
