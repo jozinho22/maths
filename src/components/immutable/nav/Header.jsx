6ddccd9389e7b67f4f56717cff2c6d5dd9775c36
jozinho <josselin.douineau.1987@gmail.com>
2022-10-21 15:56:51 +0200

    change sitemap in robots.txt

--- src/components/immutable/nav/Header.jsx
+++ src/components/immutable/nav/Header.jsx
@@ -24,7 +24,7 @@ const Header = ( {courseItems, pdfItems, gameItems} ) => {
                 <Navbar.Brand 
                     className="Clickable"
                     href="/" >
-                    MT
+                    Accueil
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav" >
