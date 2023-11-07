792d4eed1ae810992c2a73f9aa2048e778829439
jozinho <josselin.douineau.1987@gmail.com>
2023-11-07 20:50:51 +0100

    retrait geometry

--- src/components/courses/geometry/GeometryCoursesConstants.jsx
+++
@@ -1,9 +0,0 @@
-const GeometryCoursesConstants = {
-    TRIANGLE: '/le-triangle',
-    CIRCLE: '/le-cercle',
-    SPHERE: '/la-sphere',
-    QUADRILATERES: '/les-quadrilateres',
-    CONE: '/le-cone'
-}
-
-export default GeometryCoursesConstants;
